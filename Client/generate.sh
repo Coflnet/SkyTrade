eb98dba2a5cd6c2745769df23136f310376718ae
Äkwav <ekwav@coflnet.com>
2025-01-31 18:34:43 +0100

    update client generation

--- Client/generate.sh
+++ Client/generate.sh
@@ -1,14 +1,19 @@
-VERSION=0.1.1
+VERSION=0.1.3
+PACKAGE_NAME=Coflnet.Sky.Trade.Client
 
 docker run --rm -v "${PWD}:/local" --network host -u $(id -u ${USER}):$(id -g ${USER})  openapitools/openapi-generator-cli generate \
 -i http://localhost:5030/swagger/v1/swagger.json \
 -g csharp \
--o /local/out --additional-properties=packageName=Coflnet.Sky.Trade.Client,packageVersion=$VERSION,licenseId=MIT
+-o /local/out --additional-properties=packageName=$PACKAGE_NAME,packageVersion=$VERSION,licenseId=MIT,targetFramework=net6.0
 
 cd out
-sed -i 's/GIT_USER_ID/Coflnet/g' src/Coflnet.Sky.Trade.Client/Coflnet.Sky.Trade.Client.csproj
-sed -i 's/GIT_REPO_ID/SkyTrade/g' src/Coflnet.Sky.Trade.Client/Coflnet.Sky.Trade.Client.csproj
-sed -i 's/>OpenAPI/>Coflnet/g' src/Coflnet.Sky.Trade.Client/Coflnet.Sky.Trade.Client.csproj
+sed -i 's/GIT_USER_ID/Coflnet/g' src/$PACKAGE_NAME/$PACKAGE_NAME.csproj
+sed -i 's/GIT_REPO_ID/SkyTrade/g' src/$PACKAGE_NAME/$PACKAGE_NAME.csproj
+sed -i 's/>OpenAPI/>Coflnet/g' src/$PACKAGE_NAME/$PACKAGE_NAME.csproj
+
+sed -i 's@annotations</Nullable>@annotations</Nullable>\n    <PackageReadmeFile>README.md</PackageReadmeFile>@g' $path
+sed -i '34i    <None Include="../../../../README.md" Pack="true" PackagePath="\"/>' $path
 
 dotnet pack
-cp src/Coflnet.Sky.Trade.Client/bin/Debug/Coflnet.Sky.Trade.Client.*.nupkg ..
+cp src/$PACKAGE_NAME/bin/Release/$PACKAGE_NAME.*.nupkg ..
+dotnet nuget push ../$PACKAGE_NAME.$VERSION.nupkg --api-key $NUGET_API_KEY --source "nuget.org" --skip-duplicate
